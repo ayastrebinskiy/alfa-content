9a13f476bc5576dcc3706a998a2645562aad6018
root <root@relink-dev.(none)>
2016-06-23 15:41:37 +0300

    fix

--- views/main.php
+++ views/main.php
@@ -89,16 +89,16 @@
         <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KP2KC6"
                           height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <script>(function (w, d, s, l, i) {
-                        w[l] = w[l] || [];
-                        w[l].push({'gtm.start':
-                                    new Date().getTime(), event: 'gtm.js'});
-                        var f = d.getElementsByTagName(s)[0],
-                                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-                        j.async = true;
-                        j.src =
-                                '//www.googletagmanager.com/gtm.js?id=' + i + dl;
-                        f.parentNode.insertBefore(j, f);
-                    })(window, document, 'script', 'dataLayer', 'GTM-KP2KC6');</script>
+                w[l] = w[l] || [];
+                w[l].push({'gtm.start':
+                            new Date().getTime(), event: 'gtm.js'});
+                var f = d.getElementsByTagName(s)[0],
+                        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+                j.async = true;
+                j.src =
+                        '//www.googletagmanager.com/gtm.js?id=' + i + dl;
+                f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', 'GTM-KP2KC6');</script>
         <!-- End Google Tag Manager -->
         <!-- .ac-header-->
         <div class="ac-header">
@@ -1477,8 +1477,8 @@
                             <!-- /.logo-box__text-->
                         </div>
                         <div class="click-logobox">
-                          <span class="logo-info">Проект представлен:</span>
-                          <a href="//click.ru" class="click-logobox__img" target="_blank">Click.ru</a>
+                            <span class="logo-info">Проект представлен:</span>
+                            <a href="//click.ru" class="click-logobox__img" target="_blank">Click.ru</a>
                         </div>
                     </div>
                     <!-- /.ac-footer__logo-box-->
@@ -1496,6 +1496,22 @@
                 </div>
                 <div class="col-6">
                     <div class="ac-footer__right-block">
+                        <div class="utl_btn-footer">
+                            <script type="text/javascript">(function (w, doc) {
+                                    if (!w.__utlWdgt) {
+                                        w.__utlWdgt = true;
+                                        var d = doc, s = d.createElement('script'), g = 'getElementsByTagName';
+                                        s.type = 'text/javascript';
+                                        s.charset = 'UTF-8';
+                                        s.async = true;
+                                        s.src = ('https:' == w.location.protocol ? 'https' : 'http') + '://w.uptolike.com/widgets/v1/uptolike.js';
+                                        var h = d[g]('body')[0];
+                                        h.appendChild(s);
+                                    }
+                                })(window, document);
+                            </script>
+                            <div  data-background-alpha="0.0" data-buttons-color="#ffffff" data-counter-background-color="#ffffff" data-share-counter-size="15" data-top-button="false" data-share-counter-type="common" data-share-style="11" data-mode="share" data-like-text-enable="false" data-hover-effect="rotate-cw" data-mobile-view="true" data-icon-color="#ffffff" data-orientation="horizontal" data-text-color="#000000" data-share-shape="round-rectangle" data-sn-ids="fb.vk.tw.ok." data-share-size="30" data-background-color="#ffffff" data-preview-mobile="false" data-mobile-sn-ids="fb.vk.tw.wh.vb." data-pid="1537747" data-counter-background-alpha="1.0" data-following-enable="false" data-exclude-show-more="true" data-selection-enable="false" class="uptolike-buttons" ></div>
+                        </div>
                         <div class="ac-footer__mail-box"><a href="mailto:info@alfa-content.ru" class="footer-mail-link">info@alfa-content.ru</a></div>
                         <div class="ac-footer__tel-box"><a href="tel:+74959802621" class="footer-tel-link">+7 (495) 980 26 21</a></div>
                     </div>
