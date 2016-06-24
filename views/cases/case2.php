1d814895e711c182a2c7a8242fcd0b265a6ded5f
root <root@relink-dev.(none)>
2016-06-24 13:59:40 +0300

    fix

---
+++ views/cases/case2.php
@@ -0,0 +1,166 @@
+<!-- .ac-popup-modal-->
+<div id="ac-popup-case2" class="ac-popup-modal popup-modal__case"  data-case="case2">
+    <div class="ac-popup-modal__header ac-popup-modal__header-case2">
+        <div class="col-12">
+            <div class="ac-popup-modal__header-box">
+                <h2 class="ac-popup-modal__head">Кампания для онлайн-сервиса грузоперевозок</h2>
+                <div class="send-case__wrap"><a href="#ac-popup-casesend" class="send-case">Послать коллеге</a></div>
+            </div>
+        </div>
+    </div>
+    <div class="ac-popup-modal__body">
+        <div class="container-modal">
+            <div class="row">
+                <div class="col-12">
+                    <div class="wrap">
+                        <div class="left">
+                            <h2>История</h2>
+                            <p class="big-text">К нам обратилась компания из сферы грузоперевозок, что бы привлечь внимание новой аудитории и увеличить количество клиентов.</p>
+                            <p class="pl1">Высокая конкуренция на рынке частных грузоперевозок не позволяет дистанцироваться от конкурентов и эффективно привлекать новых клиентов используя каналы традиционного маркетинга (контекстная реклама, поисковое продвижение и тд), - высокая стоимость контакта, наличие фактических монополистов на рынке и т.д.</p>
+                        </div>
+                        <div class="right">
+                            <h2>Тариф</h2>
+                            <span class="img"><img src="assets/images/tariff/b-watermelon_46.png" alt="" width="128"/></span>
+                            <span class="name">Огромный охват</span>
+                            <span class="price">150 000 &#8381;</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <p class="pl pl-pb">Проанализировав входящие заказы, текущую клиентскую базу заказчика, мы сделали вывод, что несмотря на внешне “мужской” бизнес, зачастую, инициатор заказа и контактное лицо - женщина. Организация ремонта, переезда в другую квартиру или на дачу, перевозка домашних животных и другие заботы о хозяйстве часто берет на себя домохозяйка. Именно этот сегмент аудитории игнорируется большинством конкурентов. Он был выбран нами для проведения контент-маркетинговой рекламной кампании. Концепция такой кампании нашла поддержу у владельцев бизнеса.</p>
+                    <p class="pl pl-pb">Альфа-Контент были подобраны площадки в рамках обозначенного бюджета, которые сочетали широкий охват, и обладали достаточной конверсией, что бы дать заметное число переходов на сайт.</p>
+                    <p class="pl pl-pb">Концепция материалов и сами материалы - полностью уникальны и создавались для решения задачи тактических задач, кампании на выбранных площадках.</p>
+                    <p class="pl pl-pb">Согласование плана и условий размещения, его интеграция с бизнес-планом заняли около 3 недель.</p>
+                </div>
+            </div>
+            <div class="row campaign">
+                <div class="col-12">
+                    <div class="wrap">
+                        <h2>Кампания</h2>
+                        <table border="0" cellpadding="1" cellspacing="1" style="width: 100%;" class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Площадка</th>
+                                    <th scope="col">Аудитория</th>
+                                    <th scope="col">Размещение</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="col" colspan="3">Публикация в СМИ</th>
+                                </tr>
+                                <tr>
+                                    <td><span class="name-site">cosharel.livejournal.com</span><span class="name-description">Популярный женский LiveJournal- блог</span></td>
+                                    <td>Аудитория: обширная женская аудитория, лояльная, активная.</td>
+                                    <td>Размещение поста-истории с активным включением клиента + обсуждение в комментариях.</td>
+                                </tr>
+                                <tr>
+                                    <td><span class="name-site">lleo.me</span><span class="name-description">Авторский блог на интеллектуально-культурные темы, гаджеты и передовые технологии</span></td>
+                                    <td>Аудитория: 35-40 лет, активная аудитория, интересующуюся.</td>
+                                    <td>Размещение контента от автора. Автор - большой профессионал, писатель. Креативный, интеллектуальный материал. Степень доверия аудитории - очень высокая.</td>
+                                </tr>
+                                <tr>
+                                    <td> <span class="name-site">bespridanitsa.livejournal.com</span><span class="name-description">Популярный женский ЖЖ блог. Цитируемый блог с активной аудиторией.<br></span></td>
+                                    <td>Аудитория: невероятно активный блог по женской тематике.</td>
+                                    <td>Размещение материала от автора по клиенту, поддержка в комментариях.</td>
+                                </tr>
+                                <tr>
+                                    <th scope="col" colspan="3">Ролики у YouTube блогеров</th>
+                                </tr>
+                                <tr>
+                                    <td><span class="name-site">youtube.com/user/kamikadzedead</span><span class="name-description">YouTube-канал на злободневные темы</span></td>
+                                    <td>Аудитория: разнообразная. Более 680 тыс подписчиков.</td>
+                                    <td>Основательного ролика, но не про сервис, а про проблему/явление, которые можно решить с помощью вашего сервиса. Возможно ролик с участием представителя клиента.</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="row numbers">
+                <div class="col-12">
+                    <div class="wrap">
+                        <h2>Цифры</h2>
+                        <div class="numbers-img-warp">
+                            <div class="numbers-img-item">
+                                <img src="assets/images/case/case2-graph1.png" alt="Цифры" class="img" />
+                                <div class="numbers-img-item__info">
+                                    <div class="block-info-top">
+                                        <img src="assets/images/case/case-ico1.png" alt="Охват аудитории">
+                                        <div class="block-info-head">Охват аудитории</div>
+                                        <div class="block-info-n">546 213</div>
+                                    </div>
+                                    <div class="block-info-bottom">
+                                        <img src="assets/images/case/case-ico2.png" alt="Цена контакта">
+                                        <div class="block-info-head">Цена контакта</div>
+                                        <div class="block-info-n">0.27 &#8381;</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="numbers-img-item">
+                                <img src="assets/images/case/case2-graph1_2.png" alt="">
+                                <div class="numbers-img-item__info2">
+                                    <div class="block-info-top">
+                                        <img src="assets/images/case/case-ico3.png" alt="Прямые переходы на сайт">
+                                        <div class="block-info-head">Прямые переходы на сайт</div>
+                                        <div class="block-info-n">5 434</div>
+                                    </div>
+                                    <div class="block-info-bottom">
+                                        <img src="assets/images/case/case-ico4.png" alt="Цена перехода">
+                                        <div class="block-info-head">Цена перехода </div>
+                                        <div class="block-info-n">27.60 &#8381;</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <p>Прогнозируемый охват: 165 000, по факту — в 3 раза больше контактов.</p>
+                    </div>
+                    <div class="col-6">
+                        <p>Прогноз прямых переходов: 4000, по факту — на 35% больше визитов.</p>
+                        <p class="text-mini">Кампания дала десятки реальных заявок на сайт с каждого размещения. <br>
+                            Всего за 2 недели на создание и корректировку материалов;<br>
+                            Размещение контента в течение 1 месяца</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row effect">
+                <div class="col-12">
+                    <div class="wrap">
+                        <h2>Эффект от кампании</h2>
+                        <p class="big-text">В отчетах мы прогнозируем и фиксируем только аудируемые переходы, т.е. те, которые можно посчитать с помощью специальных меток в ссылках и счетчиков аналитики (Яндекс.Метрика или Goggle AdWords). Реальное количество переходов во время и после окончания рекламной кампании больше, примерно в 2-3 раза, но зафиксировать эти переходы точно не представляется возможным, пожтому, мы делаем постоценку исходя из прироста трафика, относительно среднего значения за предыдущий период.</p>
+                        <p class="pl">Важной особенностью контент-маркетинга является то, что эффект от размещения носит долговременный характер! Более популярные площадки дают более заметный и более продолжительный эффект. Наша рекомендация: провести еще одну кампанию через месяц-два для усиления пост-эффекта.</p>
+                        <h2 class="feedback-head">Отзыв клиента</h2>
+                        <p class="pl">Мы молодая компания по меркам грузоперевозок и продвигаем на рынок не стандартную бизнес-модель, активно ведем собственный маркетинг. Мало кто из подрядчиков готов думать, поэтому, мы сами ведем рекламные кампании и, я считаю, сильно экономим, т.к. сразу чувствуем обратную связь от каждого размещения. Альфа-Контент заинтересовал не стандартным подходом и возможностью “снять шоры”, что бы найти выход на новых клиентов. Мы довольны проделанной работой и рады знакомству с профессиональной командой, способной к реальной работе с потребностями заказчика, а не имитации креативной деятельности.</p>
+                        <div class="comment-block">
+                            <div class="ico"><img src="assets/images/case/comment-ico.png" alt=""/></div>
+                            <div class="comment-text">
+                                <h3 class="comment-text-head">Обратите внимание:</h3>
+                                <p>К сожалению, мы не можем публиковать реальные названия кампаний и некоторые площадки, на которых размещались рекламные материалы. Просим отнестись с пониманием.</p>
+                                <p>Если вы заинтересованы в повторении описанного эффекта или у вас похожая тематика, пожалуйста, обратитесь к нам, выбрав подходящий тариф и сославшись на этот кейс. Мы опишем все подробности и раскроем интересующие вас детали</p></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="ac-popup-modal__footer">
+        <div class="ac-popup-modal__footer-warp">
+            <div class="ac-popup-modal__footer-warp__bg">
+                <div class="container-modal">
+                    <div class="row">
+                        <div class="col-12">
+                            <h2 class="ac-popup-modal__footer-head">Хочу такое же!</h2>
+                            <h3 class="ac-head__mini ac-popup-modal__footer-head_small">и даже лучше</h3>
+                            <div class="btn-wrap"><a href="#ac-popup-mini" class="btn">подать заявку</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /.ac-popup-modal-->
