cd48938efccab4ac085a9cc5f679f1e1031056f9
Sergey Glazov <yaglazov@gmail.com>
2016-09-08 15:15:04 +0300

    добавлен поап-блок для 4-го кейса, добавленка картинка-шапка для 4-го кейса

---
+++ views/cases/case4.php
@@ -0,0 +1,33 @@
+<!-- .ac-popup-modal-->
+<div id="ac-popup-case4" class="ac-popup-modal popup-modal__case mfp-hide"  data-case="4">
+    <div class="ac-popup-modal__header ac-popup-modal__header-case4">
+        <div class="col-12">
+            <div class="ac-popup-modal__header-box">
+                <h2 class="ac-popup-modal__head"><?=$case['post_title']?></h2>
+                <div class="send-case__wrap"><a href="#ac-popup-casesend" class="send-case">Послать коллеге</a></div>
+            </div>
+        </div>
+    </div>
+    <div class="ac-popup-modal__body">
+        <div class="container-modal">
+            <?=$case['post_content']?>
+        </div>
+    </div>
+    <div class="ac-popup-modal___social"></div>
+    <div class="ac-popup-modal__footer">
+        <div class="ac-popup-modal__footer-warp">
+            <div class="ac-popup-modal__footer-warp__bg">
+                <div class="container-modal">
+                    <div class="row">
+                        <div class="col-12">
+                            <h2 class="ac-popup-modal__footer-head">Хочу такое же!</h2>
+                            <h3 class="ac-head__mini ac-popup-modal__footer-head_small">и даже лучше</h3>
+                            <div class="btn-wrap"><a href="#ac-popup-mini" class="btn">подать заявку</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /.ac-popup-modal-->
